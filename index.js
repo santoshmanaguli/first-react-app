32becffeb91b16e2fa451a9b42d1dbf54da25267
Santosh Managuli <rocksantosh15@gmail.com>
2023-02-18 19:46:09 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+function RenderThis(){
+    return(
+        <div>
+        <h2> Javascipt Framework</h2>
+        <ul>
+            <li>React</li>
+            <li>Vue</li>
+            <li>Angular</li>
+            <li>Bolt.js</li>
+        </ul>
+        </div>
+    )
+}
+
+
+ReactDOM.render(RenderThis(), document.getElementById('root')); 
